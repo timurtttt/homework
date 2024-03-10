f17c8b82bc27a594138d1824660a83227d12da7e
timurtttt <tima.eshankul@gmail.com>
2024-03-10 12:57:50 +0600

    dz1

---
+++ js/home_works.js
@@ -0,0 +1,31 @@
+// let gmailInpuit = document.getElementById("gmail_input");
+// let gmailButton = document.getElementById("gmail_button");
+// let gmailResult = document.getElementById("gmail_result");
+
+// const regExp = /@gmail.com/;
+
+// gmailButton.addEventListener("click", () => {
+//   if (regExp.test(gmailInpuit.value)) {
+//     gmailResult.innerHTML = "ok";
+//     gmailResult.style.color = "green";
+//   } else {
+//     gmailResult.innerHTML = "not ok";
+//     gmailResult.style.color = "red";
+//   }
+// });
+
+// let divchild = document.getElementById("child_block");
+
+// const childBlock = document.querySelector(".child_block");
+
+// let positionChild = 0;
+
+// const moveBlock = () => {
+//   if (positionChild < 450) {
+//     positionChild++;
+//     childBlock.style.left = `${positionChild}px`;
+
+//     requestAnimationFrame(moveBlock);
+//   }
+// };
+// moveBlock();
